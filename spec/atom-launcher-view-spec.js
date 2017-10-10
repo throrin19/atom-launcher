af9b7bd3b996038bb86e0bfefb6c2cc80632c523
Benjamin Besse <benjamin.besse02@gmail.com>
2017-10-10 16:43:13 +0200

    first commit

---
+++ spec/atom-launcher-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomLauncherView from '../lib/atom-launcher-view';
+
+describe('AtomLauncherView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
